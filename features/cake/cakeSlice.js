bfbca1ff9e326a6207d29885a3fcc25c799cc3c8
emmeiwhite <emmeiwhite@gmail.com>
2025-06-05 23:21:41 +0530

    rtk-2 create cakeSlice

---
+++ features/cake/cakeSlice.js
@@ -0,0 +1,15 @@
+const createSlice = require('@reduxjs/toolkit').createSlice
+
+const initialState = {
+  numOfCakes: 30
+}
+
+const cakeSlice = createSlice({
+  name: 'cake',
+  initialState,
+  reducers: {
+    addCake: (state, action) => {
+      state.numOfCakes--
+    }
+  }
+})
