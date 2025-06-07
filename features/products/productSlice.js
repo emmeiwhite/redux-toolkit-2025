6a349d2a9b6a1d920f8accf371ba877b33219715
emmeiwhite <emmeiwhite@gmail.com>
2025-06-07 13:15:43 +0530

    rtk-16 initialState for Async stuff !

--- features/products/productSlice.js
+++ features/products/productSlice.js
@@ -1,5 +1,11 @@
 const createSlice = require('@reduxjs/toolkit').createSlice
 
+const initialState = {
+  loading: false,
+  products: [],
+  error: ''
+}
+
 const productSlice = createSlice({
   name: 'product',
   initialState
