753c160745c1a9392e27ec50bec508a48fe754cf
emmeiwhite <emmeiwhite@gmail.com>
2025-06-07 13:36:25 +0530

    rtk-17 step-1 define async thunk

--- features/products/productSlice.js
+++ features/products/productSlice.js
@@ -1,3 +1,6 @@
+const { createAsyncThunk } = require('@reduxjs/toolkit')
+const { default: axios } = require('axios')
+
 const createSlice = require('@reduxjs/toolkit').createSlice
 
 const initialState = {
@@ -6,6 +9,13 @@ const initialState = {
   error: ''
 }
 
+/** --- 1) Define the Async Thunk | To perform async operation --- */
+const fetchProducts = createAsyncThunk('product/fetchProducts', () => {
+  return axios.get('https://fakestoreapi.com/products').then(res => console.log(res.data))
+})
+// Async Fetch !!!
+
+// For async-call, we do not need to use reducers
 const productSlice = createSlice({
   name: 'product',
   initialState
